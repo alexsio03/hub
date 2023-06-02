49e1e2be2146e72584315c4e35f99d65bd074b80
Alex Warda <alexsio03@gmail.com>
2023-06-01 19:28:11 -0700

    added simple navbar w/ linking

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function Login() {
+  return (
+    <div>
+      <h1 className="title">
+        Go <Link href="/">home</Link>
+      </h1>
+      
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
-    <main>
-      Hello World!
-    </main>
+    <div className="m-8 flex justify-center space-x-96">
+      <h1 className="title">
+        <Link href="/">Home</Link>
+      </h1>
+      <h1 className="title">
+        <Link href="market">Market</Link>
+      </h1>
+      <h1 className="title">
+        <Link href="trades">Trades</Link>
+      </h1>
+      <h1 className="title">
+        <Link href="login">Login</Link>
+      </h1>
+    </div>
   )
 }
