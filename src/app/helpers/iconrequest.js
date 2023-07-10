336dc3fdf12bc9192c3b83149857a87ac135884c
Nate Kim <enemeski@gmail.com>
2023-07-10 16:36:53 -0700

    JSON database thing for item icons moved the json/database check to its own helper file better encoding and parsing so more icons are properly loaded buff price setting is try/catch to prevent site crash when buff price is null updated skinprices.json

--- src/app/helpers/iconrequest.js
+++ src/app/helpers/iconrequest.js
@@ -1,33 +1,56 @@
 import axios from 'axios';
+import GetIconFromJSON from './checkiconjson.js';
 
 var fullURL;
 
 // Finds icon of a skin when given the name
 export default async function IconRequest(skinName){
-    const url1 = 'https://steamcommunity.com/market/listings/730/'
-    const urlData = EncodeParentheses(encodeURIComponent(skinName));
-    const url = url1 + urlData;
+  if (GetIconFromJSON(skinName) != undefined){
+    const fd = require('fs');
+    fd.writeFile('src/app/helpers/HAPPEN.txt', "DUPLICATE WAS CAUGHT", {flag: 'a+'}, (err) => {
+      if (err) throw err;
+    });
 
-    await axios.get(url)
-    .then((getResponse) => {
-      let data = getResponse.data;
-      let startPos = data.search('https://community.cloudflare.steamstatic.com/economy/image/-9');
-      let endPos = data.indexOf('>', startPos);
+    return GetIconFromJSON(skinName);
+  }
 
-      // Example: https://community.cloudflare.steamstatic.com/economy/image/-9a81dlWLwJ2U . . . /360fx360f>
-      // endPos-1 takes off the >
-      // endPos-10 takes off the sizing so that it can be changed to 330x192
-      const skinIconURL = data.substring(startPos, endPos-10);
-      fullURL = skinIconURL + "330x192";
-    })
-    .catch(err=>console.log("\n\nERROR steamrequest.js: " + err + "\n\n"))
+  const url1 = 'https://steamcommunity.com/market/listings/730/'
+  const urlData = ExtraEncode(encodeURIComponent(skinName));
+  const url = url1 + urlData;
 
-    return fullURL;
+  const fs = require('fs');
+  fs.writeFile('src/app/helpers/FULLHTML.txt', "\n" + url + "\n\n", {flag: 'a+'}, (err) => {
+    if (err) throw err;
+  });
+
+  await axios.get(url)
+  .then((getResponse) => {
+    let data = getResponse.data;
+    let startPos = data.indexOf('https://community.cloudflare.steamstatic.com/economy/image/');
+    let endPos = data.indexOf('>', startPos);
+
+    // Example: https://community.cloudflare.steamstatic.com/economy/image/-9a81dlWLwJ2U . . . /360fx360f>
+    // endPos-1 takes off the >
+    // endPos-10 takes off the sizing so that it can be changed to 330x192
+    const skinIconURL = data.substring(startPos, endPos-10);
+    fullURL = skinIconURL + "330x192";
+
+    // Uncomment this to print full html response(s) to FULLHTML.txt
+    // const fs = require('fs');
+    // fs.writeFile('src/app/helpers/FULLHTML.txt', data + "\n\n\n", {flag: 'a+'}, (err) => {
+    //   if (err) throw err;
+    // });
+  })
+  .catch(err=>console.log("\n\nERROR iconrequest.js: " + err + "\n\n"))
+
+  return fullURL;
 }
 
-// Changes parentheses to comply with steam url convention
-function EncodeParentheses(str){
+// Extra character encoding to comply with steam url convention
+function ExtraEncode(str){
   const opening = str.replace("(", "%28");
   const closing = opening.replace(")", "%29");
-  return closing;
+  const apost = closing.replace("\'", "%27")
+  const revert = apost.replace("%2F", "-");
+  return revert;
 }
