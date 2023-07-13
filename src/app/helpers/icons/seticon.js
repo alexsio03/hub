20c962974bcb1fdc36b50ed4531ea6108c2748ca
Nate Kim <enemeski@gmail.com>
2023-07-12 21:52:45 -0700

    More comments removed weird gpt stuff from ItemcardR cleaned up/fixed checkiconjson & seticon

--- src/app/helpers/icons/seticon.js
+++ src/app/helpers/icons/seticon.js
@@ -1,7 +1,4 @@
-import data from '../wakInv.json'; // Won't be needed once inventory accessing works with loadinventory.js
-
 // Returns large icon if it exists, normal icon otherwise
-export default function SetIcon(item){
-  const description = data['rgDescriptions'][item];
-  return description.icon_url_large || description.icon_url;
+export default function SetIcon(item) {
+  return item.icon_url_large || item.icon_url;
 }
